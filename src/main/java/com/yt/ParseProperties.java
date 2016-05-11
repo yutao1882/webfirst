44a277937815f2b2bded52abd5586a0dd28d7c29
Owner <Owner@Owner-PC>
2016-05-11 14:56:12 +0800

    20160511 parseProperty

--- src/main/java/com/yt/ParseProperties.java
+++ src/main/java/com/yt/ParseProperties.java
@@ -1,26 +1,53 @@
 package com.yt;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
 public class ParseProperties {
 
 	private Properties pro = new Properties();
 	String value = null;
-	
-	
-	private void loadProperties(String propertiesPath){
+
+	public ParseProperties(String propertiesPath) {
+		this.loadProperties(propertiesPath);
+	}
+
+	private void loadProperties(String propertiesPath) {
+
 		try {
 			InputStream in = new FileInputStream(propertiesPath);
+			InputStreamReader isr = new InputStreamReader(in);
+			BufferedReader br = new BufferedReader(isr);
+			pro.load(br);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-//		pro.load(reader);
-		
+
+	}
+
+	public String getValue(String keyword) {
+
+		value = pro.getProperty(keyword);
+
+		try {
+			value = new String(value.getBytes("UTF-8"), "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return value;
+
 	}
 
 }
